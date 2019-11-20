cff8dd4bb5beb5eb7840c8b32abebb669f30d826
TsyrulAlexander <48672629+TsyrulAlexander@users.noreply.github.com>
2019-11-20 18:35:57 +0200

    add project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyGame.Scene;
+
+namespace MyGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main() {
+			using (var game = new MainScene()) {
+				game.Run();
+			}
+		}
+    }
+}
