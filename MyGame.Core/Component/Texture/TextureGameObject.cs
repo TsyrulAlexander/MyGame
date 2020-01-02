5c63eac608ef46b337767223c9b3a313f9464873
TsyrulAlexander <48672629+TsyrulAlexander@users.noreply.github.com>
2020-01-02 21:03:49 +0200

    BugFix TextureGameObject

--- MyGame.Core/Component/Texture/TextureGameObject.cs
+++ MyGame.Core/Component/Texture/TextureGameObject.cs
@@ -1,41 +1,22 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using MyGame.Core.Component.GameObject;
-using MyGame.Core.Scene;
 
 namespace MyGame.Core.Component.Texture
 {
 	[Serializable]
 	public class TextureGameObject : BaseGameObject {
-		private readonly ContentManager _contentManager;
-		private string _textureName;
 		protected SpriteBatch SpriteBatch { get; } = GameServices.GetService<SpriteBatch>();
-		public string TextureName {
-			get => _textureName;
-			set {
-				_textureName = value;
-				OnPropertyChanged();
-			}
-		}
 		public Texture2D Texture { get; set; }
 		public TextureGameObject(SerializationInfo info, StreamingContext context): base(info, context) {
 			
 		}
-		public TextureGameObject(string textureName) {
-			TextureName = textureName;
-			_contentManager = SceneManager.CurrentScene.ContentManager;
+		public TextureGameObject(Texture2D texture) {
+			Texture = texture;
 		}
-		protected override void OnPropertyChanged([CallerMemberName]string propertyName = null) {
-			if (propertyName == nameof(TextureName)) {
-				Texture = _contentManager.Load<Texture2D>(TextureName);
-				Size = new Vector2(Texture.Width, Texture.Height);
-			}
-			base.OnPropertyChanged(propertyName);
-		}
+		
 		public override void Draw( GameTime gameTime) {
 			base.Draw(gameTime);
 			if (!Visible || Texture == null) {
