4a604e816701303fe26817139efb017026775767
TsyrulAlexander <48672629+TsyrulAlexander@users.noreply.github.com>
2020-01-02 21:00:02 +0200

    remove old files

--- MyGame.Core/Component/GameObject/Container/Container.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MyGame.Core.Component.GameObject.Container {
-	public class Container : BaseGameObject {
-		public List<ContainerItem> Items { get; set; }
-	}
-}
